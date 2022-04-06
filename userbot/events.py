63f8e6886196a1c793127393f083c51fe2e1d35b
UserKontol <101336638+UserKontol@users.noreply.github.com>
2022-04-06 05:01:39 +0000

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -194,8 +194,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/SharingUserbot)"
+                    text = "**✘ ADAM-USERBOT ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/wibu_telegram)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
